b30bdd360d8916f649cb72f617c652196a17bb6e
jonquixote <jehawley@gmail.com>
2016-05-07 00:40:42 -0700

    fuck ya

--- RockPaperScissors/GameController.swift
+++ RockPaperScissors/GameController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class GameController: UIViewController {
 
-
+    
+    
     @IBOutlet weak var CPU: UILabel!
     @IBOutlet weak var You: UILabel!
     @IBOutlet weak var Result: UILabel!
@@ -18,13 +19,67 @@ class GameController: UIViewController {
     @IBOutlet weak var CPUPick: UIImageView!
     @IBAction func Rock(sender: AnyObject) {
         YouPick.image = UIImage(named: "rock-clipart-3.png")
+        self .computerChoice()
+        ButtonPressed = 1
+        self .result()
     }
     @IBAction func Paper(sender: AnyObject) {
         YouPick.image = UIImage(named: "parchment-scroll-clip-art_557532.jpg")
+        self .computerChoice()
+        ButtonPressed = 2
+        self .result()
     }
     @IBAction func Scissors(sender: AnyObject) {
         YouPick.image = UIImage(named: "scissors-clip-art-cartoon-illustration-funny-32879166.png")
+        self .computerChoice()
+        ButtonPressed = 3
+        self .result()
     }
+    
+    
+    let random = arc4random_uniform(3) + 1
+    func computerChoice () {
+        if random == 1 {
+            CPUPick.image = UIImage(named: "rock-clipart-3.png")
+        }
+        if random == 2 {
+            CPUPick.image = UIImage(named: "parchment-scroll-clip-art_557532.jpg")
+        }
+        if random == 3 {
+            CPUPick.image = UIImage(named: "scissors-clip-art-cartoon-illustration-funny-32879166.png")
+        }
+    }
+    
+    var ButtonPressed: Int?
+    var winner: Int?
+    func result() {
+        if ButtonPressed == 1 {
+            if (random==1) {winner=1}
+            if (random==2) {winner=3}
+            if (random==3) {winner=2}
+        }
+        if ButtonPressed == 2 {
+            if (random==1) {winner=2}
+            if (random==2) {winner=1}
+            if (random==3) {winner=3}
+        }
+        if ButtonPressed == 3 {
+            if (random==1) {winner=3}
+            if (random==2) {winner=2}
+            if (random==3) {winner=1}
+        }
+        if winner == 1 {
+            Result.text = "Tie"
+        }
+        if winner == 2 {
+            Result.text = "You win!"
+        }
+        if winner == 3 {
+            Result.text = "You suck!"
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
