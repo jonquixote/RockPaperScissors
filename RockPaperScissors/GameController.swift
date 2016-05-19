cadd28f5922443f1027f539e261b5a632ff33fdb
jonquixote <jehawley@gmail.com>
2016-05-18 21:12:02 -0700

    ok! switched to switch in swift

--- RockPaperScissors/GameController.swift
+++ RockPaperScissors/GameController.swift
@@ -17,74 +17,100 @@ class GameController: UIViewController {
     @IBOutlet weak var Result: UILabel!
     @IBOutlet weak var YouPick: UIImageView!
     @IBOutlet weak var CPUPick: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Result.text = "Play!"
+        // Do any additional setup after loading the view.
+    }
+    
     @IBAction func Rock(sender: AnyObject) {
         YouPick.image = UIImage(named: "rock-clipart-3.png")
-        self .computerChoice()
         ButtonPressed = 1
         self .result()
     }
     @IBAction func Paper(sender: AnyObject) {
         YouPick.image = UIImage(named: "parchment-scroll-clip-art_557532.jpg")
-        self .computerChoice()
         ButtonPressed = 2
         self .result()
     }
     @IBAction func Scissors(sender: AnyObject) {
         YouPick.image = UIImage(named: "scissors-clip-art-cartoon-illustration-funny-32879166.png")
-        self .computerChoice()
         ButtonPressed = 3
         self .result()
     }
     
     
-    let random = arc4random_uniform(3) + 1
-    func computerChoice () {
+    func computerChoice () -> UInt32 {
+        let random = arc4random_uniform(3) + 1
         if random == 1 {
             CPUPick.image = UIImage(named: "rock-clipart-3.png")
         }
         if random == 2 {
             CPUPick.image = UIImage(named: "parchment-scroll-clip-art_557532.jpg")
         }
-        if random == 3 {
+        else if random == 3 {
             CPUPick.image = UIImage(named: "scissors-clip-art-cartoon-illustration-funny-32879166.png")
         }
+        return random
     }
     
     var ButtonPressed: Int?
     var winner: Int?
     func result() {
-        if ButtonPressed == 1 {
-            if (random==1) {winner=1}
-            if (random==2) {winner=3}
-            if (random==3) {winner=2}
+        let compResult = self.computerChoice()
+        
+        switch (ButtonPressed!, compResult) {
+        case (1,1):
+            winner = 1
+        case (1,2):
+            winner = 3
+        case (1,3):
+            winner = 2
+        case (2,1):
+            winner = 2
+        case (2,2):
+            winner = 1
+        case (2,3):
+            winner = 3
+        case (3,1):
+            winner = 3
+        case (3,2):
+            winner = 2
+        case (3,3):
+            winner = 1
+        default:
+            break
         }
-        if ButtonPressed == 2 {
-            if (random==1) {winner=2}
-            if (random==2) {winner=1}
-            if (random==3) {winner=3}
+/*
+      if ButtonPressed==1 {
+            if (compResult==1) {winner=1}
+            if (compResult==2) {winner=3}
+            if (compResult==3) {winner=2}
         }
-        if ButtonPressed == 3 {
-            if (random==1) {winner=3}
-            if (random==2) {winner=2}
-            if (random==3) {winner=1}
+        if ButtonPressed==2 {
+            if (compResult==1) {winner=2}
+            if (compResult==2) {winner=1}
+            if (compResult==3) {winner=3}
         }
-        if winner == 1 {
-            Result.text = "Tie"
+        else if ButtonPressed==3 {
+            if (compResult==1) {winner=3}
+            if (compResult==2) {winner=2}
+            if (compResult==3) {winner=1}
         }
-        if winner == 2 {
-            Result.text = "You win!"
-        }
-        if winner == 3 {
-            Result.text = "You suck!"
-        }
-    }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+*/
 
-        // Do any additional setup after loading the view.
+        if winner==1 {
+            Result.text="Tie"
+        }
+        if winner==2 {
+            Result.text="You win!"
+        }
+        else if winner==3 {
+            Result.text="You suck!"
+        }
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
